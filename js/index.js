df9436cbed1f7fecba7019c678685901a4bd3380
George Boots <ggboots.99@gmail.com>
2020-07-09 11:20:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 
-const main = document.getElementById('main');
-
-main.addEventListener('click', function(event){
-  alert('I was clicked');
-});
 
 const input = document.querySelector('input')
 
