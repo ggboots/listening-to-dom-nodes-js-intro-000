338d14bac0ff26c0692a8af0e6930b3012840f5a
George Boots <ggboots.99@gmail.com>
2020-07-09 11:18:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -26,8 +26,6 @@ for (let i = 0; i < divs.length; i++){
   divs[i].addEventListener('click', bubble);
 }
 
-const divs = document.querySelectorAll('div');
-
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim()+ ' captured');
 }
